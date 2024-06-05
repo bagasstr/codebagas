0cbd663da624327e44bd934398f01ced3a2ea4a4
bagasstr <bagassatrio2805@gmail.com>
2024-06-06 02:12:26 +0700

    fix route

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,8 +1,8 @@
 import { sanityClient } from "@/lib/utils";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export const runtime = "edge";
-export async function GET(res: NextResponse, req: NextRequest) {
+export async function GET(): Promise<NextResponse> {
   try {
     const query = `*[_type == "projects"]{_id, _type, title, slug, short_desc, long_desc, tools, github, demo, thumbnail, image}`;
     const data = await sanityClient.fetch(query);

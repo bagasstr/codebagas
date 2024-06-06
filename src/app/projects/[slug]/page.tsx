d5109481c03dbf24b26d2d505aca5cf1bc5ad5f3
bagasstr <bagassatrio2805@gmail.com>
2024-06-06 09:55:42 +0700

    fix

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,14 +1,11 @@
 import { getSingleData } from "@/app/utils/helpers";
 import { ProjectsPortableText } from "@/components/ProjectsPortableText";
 import { PortableText } from "next-sanity";
-import Image from "next/image";
-import React, { HtmlHTMLAttributes } from "react";
-import { urlForImage } from "../../../../sanity/lib/image";
 
-const components: any = {};
 const page = async ({ params }: { params: { slug: string } }) => {
   let slug = params.slug;
   const data = await getSingleData(slug);
+  console.log(data);
 
   return (
     <>
